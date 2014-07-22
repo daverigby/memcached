cdf23c39cdaf80e12c60d8c8f51ffaa46e4501a7
Dave Rigby <daver@couchbase.com>
2014-07-22 17:47:07 +0100

    jemalloc: Implement mem_used tracking using experimental hooks
    Make use of the experimental jemalloc new/delete hooks (from:
    https://github.com/daverigby/jemalloc/tree/dev) to allow memcached to
    track mem_used when using jemalloc as the memory allocator.
    
    Change-Id: Iedc51fb68430b78eb2558925a458c3cc640d3943

--- tests/tcmalloc_test.cc
+++ tests/tcmalloc_test.cc
@@ -5,6 +5,7 @@
 //
 #include "config.h"
 #include <cassert>
+#include <cstdlib>
 
 #include "daemon/alloc_hooks.h"
 
@@ -14,28 +15,95 @@ extern "C" {
     static void NewHook(const void* ptr, size_t) {
         if (ptr != NULL) {
             void* p = const_cast<void*>(ptr);
-            alloc_size = mc_get_allocation_size(p);
+            size_t sz = mc_get_allocation_size(p);
+            alloc_size += sz;
         }
     }
 
     static void DeleteHook(const void* ptr) {
         if (ptr != NULL) {
             void* p = const_cast<void*>(ptr);
-            alloc_size = mc_get_allocation_size(p);
+            size_t sz = mc_get_allocation_size(p);
+            alloc_size -= sz;
         }
     }
 
     static void TestThread(void* arg) {
         alloc_size = 0;
 
-        char *p = new char[100];
-        assert(alloc_size > 0);
-        size_t allocated = alloc_size;
+        // Test new / delete
+        {
+            char *p = new char();
+            assert(alloc_size > 0);
 
-        alloc_size = 0;
-        delete []p;
+            delete p;
+            assert(alloc_size == 0);
+        }
 
-        assert(alloc_size == allocated);
+        // Test new[] / delete[]
+        {
+            char *p = new char[100];
+            assert(alloc_size >= 100);
+
+            delete []p;
+
+            assert(alloc_size == 0);
+        }
+
+        // Test malloc() / free()
+        {
+            char *p = static_cast<char*>(malloc(sizeof(char) * 10));
+            assert(alloc_size >= 10);
+
+            free(p);
+            assert(alloc_size == 0);
+        }
+
+        // Test realloc()
+        {
+            char *p = static_cast<char*>(malloc(1));
+            assert(alloc_size >= 1);
+
+            // Allocator may round up allocation sizes; so it's hard to
+            // accurately predict how much alloc_size will increase. Hence
+            // we just increase by a "large" amount and check at least half that
+            // increment.
+            size_t prev_size = alloc_size;
+            p = static_cast<char*>(realloc(p, sizeof(char) * 100));
+            assert(alloc_size >= (prev_size + 50));
+
+            prev_size = alloc_size;
+            p = static_cast<char*>(realloc(p, 0));
+            assert(alloc_size < prev_size);
+
+            prev_size = alloc_size;
+            char* q = static_cast<char*>(realloc(NULL, 10));
+            assert(alloc_size >= prev_size + 10);
+
+            free(p);
+            free(q);
+            assert(alloc_size == 0);
+        }
+
+        // Test calloc()
+        {
+            char *p = static_cast<char*>(calloc(sizeof(char), 20));
+            assert(alloc_size >= 20);
+
+            free(p);
+            assert(alloc_size == 0);
+        }
+
+        // Test posix_memalign()
+        {
+            void *p = NULL;
+            int res = posix_memalign(&p, 256, 100);
+            assert(res == 0);
+            assert(alloc_size >= 100);
+
+            free(p);
+            assert(alloc_size == 0);
+        }
     }
 }
 
